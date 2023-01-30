d986683eef99446dde74205154f1b49f91da5d1e
Lucas Gover <lgover@pugetsound.edu>
2023-01-30 08:00:33 +0000

    everything is implemented—badly though

--- heap.c
+++ heap.c
@@ -1,13 +1,32 @@
 /*
  * heapsort.c
  *
- *  Created on: Jul 1, 2013
- *      Author:
+ *  Created on: Jan 29, 2023
+ *      Author: lgover
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include "employee.h"
 #include "heap.h"
+
+/**
+ * Gets the index of the left child of a given index
+ * 
+ * @param i index you want the left child of
+ * @return the index of the left child
+*/
+int left(int i){
+	return i*2 + 1;
+}
+/**
+ * Gets the index of the right child of a given index
+ * 
+ * @param i index you want the right child of
+ * @return the index of the right child
+*/
+int right(int i){
+	return i*2 + 2;
+}
 
 /**
  * Sorts a list of n employees in descending order
@@ -17,12 +36,19 @@
  */
 void heapSort(Employee *A, int n)
 {
-	// TODO - BuildHeap on the heap
+	// BuildHeap on the heap
+	printf("hi");
+	buildHeap(&A[0],n);
 
-	// TODO - while n > 0:
-	// TODO - swap A[n-1] with A[0], since A[0] is the smallest number.
+	int m = n;
+	while (m > 0){
+		//swap A[n-1] with A[0], since A[0] is the smallest number.
+		swap(&A[n-1],&A[0]);
+		m--;
+		heapify(&A[0],0,m);
 	// TODO - A[n-1] now sorted in place, so decrement n
 	// TODO - Heapify the elements from A[0] up to A[n-1] (which leaves the newly sorted element alone)
+	}
 }
 
 /**
@@ -35,7 +61,10 @@ void heapSort(Employee *A, int n)
  */
 void buildHeap(Employee *A, int n)
 {
-	// TODO - heapify() every element from A[n/2] down-to A[0]
+	// heapify() every element from A[n/2] down-to A[0]
+	for(int i = n/2; i >= 0; i--){
+		heapify(&A[0],i,n);
+	}
 }
 
 /**
@@ -46,17 +75,33 @@ void buildHeap(Employee *A, int n)
  * @param	i	Index of current element to heapify
  * @param	n	Size of the heap
  */
+/**
+ * We want to start with A[i] and percolate it downward
+ * if it is greater than either left or right child.
+ *
+ * @param	*A	Pointer to the list of employees
+ * @param	i	Index of current element to heapify
+ * @param	n	Size of the heap
+ */
 void heapify(Employee *A, int i, int n)
 {
-	// TODO - get index of left child of element i
-	// TODO - get index of right child of element i
+	// Determine which child has a smaller salary. We'll call the index of this
+	// element: "smaller"
+	int smaller;
+	if (right(i) < n && A[right(i)].salary < A[left(i)].salary){
+		smaller = right(i);
+	}else{
+		smaller = left(i);
+	}
 
-	// TODO - determine which child has a smaller salary. We'll call the index of this
-	//		element: "smaller"
-
-	// TODO - recursively check if the salary at A[i] > the salary at A[smaller]. If it is, swap the two.
-	//			Then recursively heapify A[smaller].
-	// TODO - Continue recursion as long as i is within range AND either right_child and left_child are still within range.
+	// Recursively check if the salary at A[i] > the salary at A[smaller]. If it is, swap the two.
+	// Then recursively heapify A[smaller].
+	if (i < n && A[i].salary > A[smaller].salary) {
+		Employee temp = A[i];
+		A[i] = A[smaller];
+		A[smaller] = temp;
+		heapify(A, smaller, n);
+	}
 }
 
 /**
@@ -64,9 +109,13 @@ void heapify(Employee *A, int i, int n)
  * @param *e1 An Employee
  * @param *e2 Another Employee to swap with
  */
-void swap(Employee *e1, Employee *e2)
-{
-	// TODO
+void swap(Employee *e1, Employee *e2){
+	// Store e1's data in a temp struct
+    Employee temp = *e1;
+    // Copy e2's data into e1
+    *e1 = *e2;
+    // Copy temp (e1's original data) into e2
+    *e2 = temp;
 }
 
 /**
@@ -76,5 +125,8 @@ void swap(Employee *e1, Employee *e2)
  */
 void printList(Employee *A, int n)
 {
-	// TODO
+	for(int i = 0; i < n-1; i++){
+		printf("[id=%s sal=%u], ",A[i].name,A[i].salary);
+	}
+	printf("[id=%s sal=%u]\n",A[n-1].name,A[n-1].salary);
 }
--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 #include "employee.h"
 #include "heap.h"
 
 int main(int argc, char *argv[])
 {
 	//TODO
+	Employee E[5];
+	strcpy(E[0].name,"1");
+	strcpy(E[1].name,"2");
+	strcpy(E[2].name,"3");
+	strcpy(E[3].name,"4");
+	strcpy(E[4].name,"5");
+
+	E[0].salary = 5;
+	E[1].salary = 4;
+	E[2].salary = 3;
+	E[3].salary = 2;
+	E[4].salary = 1;
+
+	swap(&E[0],&E[1]);
+	printf("hi");
+	//heapSort(&E[0],5);
+	printList(&E[0],5);
+
 	
 	return 0;
 }
